b103ac6929e6ef53ee4a2c089953f9706f56bd23
opilane <opilane@HP-CZC2349HT9>
2023-03-24 14:57:55 +0200

    register

--- registerArkadiKorotitshTARpv22.py
+++ registerArkadiKorotitshTARpv22.py
@@ -3,9 +3,9 @@ from registermoduul import *
 logins={}
 passwords={} 
 aken=Tk() 
-aken.geometry("600x750")
+aken.geometry("500x425")
 aken.title("Register") 
-aken.iconbitmap("icon.ico")
+aken.iconbitmap("135620.ico")
 aken.configure(bg="darkorange")
 def end():
     aken.destroy() 
@@ -15,15 +15,16 @@ def end():
 
 
 
-btn1=Button(aken, text="registreerimine", font="Arial 24",fg="white",bg="orange",relief=RAISED,command=registreerimine)
-btn2=Button(aken, text="autoriseerimine", font="Arial 24",relief=RAISED,command=autoriseerimine)
-btn3=Button(aken, text="nime voi parooli muutmine", font="Arial 24",relief=RAISED,command=muuda)
-btn4=Button(aken, text="unustanud parooli taastamine", font="Arial 24",relief=RAISED,command=help)
-btn5=Button(aken, text="lopetamine", font="Arial 24",relief=RAISED,command=end)
+btn1=Button(aken, text="registreerimine", font="Arial 24",fg="orange4",bg="orange",relief=RAISED,command=registreerimine)
+btn2=Button(aken, text="autoriseerimine", font="Arial 24",fg="orange4",bg="orange",relief=RAISED,command=autoriseerimine)
+btn3=Button(aken, text="nime voi parooli muutmine", font="Arial 24",fg="orange4",bg="orange",relief=RAISED,command=muuda)
+btn4=Button(aken, text="unustanud parooli taastamine", font="Arial 24",fg="orange4",bg="orange",relief=RAISED,command=help)
+btn5=Button(aken, text="lopetamine", font="Arial 24",fg="limegreen",bg="green4",relief=RAISED,command=end)
 
 
 
-ob=[btn1,btn2,btn3,btn4,btn5]
+btn5.place(rely=1.0, relx=1.0, x=0, y=0, anchor=SE)
+ob=[btn1,btn2,btn3,btn4]
 for i in range(len(ob)):
     ob[i].pack()
 aken.mainloop()
